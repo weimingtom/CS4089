eedafc029eecb70dfcdd6bb747684c66eabb215b
Shrimadhav U K <shrimadhavuk@gmail.com>
2016-11-25 06:31:55 +0530

    condition to get out of infinite loop

--- FaceRecognition/main.py
+++ FaceRecognition/main.py
@@ -6,15 +6,19 @@ import os
 import sys
 import time
 
-def majority(mylist):
+# http://stackoverflow.com/a/189664/4723940
+class GetOutOfLoop(Exception) :
+    pass
+
+def majority(mylist) :
     '''
     Takes a list and returns an element which has highest frequency in the given list.
     '''
     myset = set(mylist)
     ans = mylist[0]
     ans_f = mylist.count(ans)
-    for i in myset:
-        if mylist.count(i) > ans_f:
+    for i in myset :
+        if mylist.count(i) > ans_f :
             ans = i
             ans_f = mylist.count(i)
     return ans
@@ -31,17 +35,17 @@ def take_images_for_training(input_images, entity_name) :
     path_to_save_images = input_images + os.path.sep
     capture = cv2.VideoCapture(0)
     print("keep your face in different angles")
-    while(True):
+    while(True) :
         ret , frame = capture.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # cv2.imshow('frame',gray)
         print("writing to file "+str(i)+".png")
         cv2.imwrite(path_to_save_images + str(entity_name + '_' + i) + '.png', gray)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q') :
             break
         i = int(i)
         i = i + 1
-        if i >= 10:
+        if i >= 10 :
             break
         i = str(i)
         print("sleeping for five seconds")
@@ -58,17 +62,19 @@ def detect_faces_in_image(input_images, output_faces) :
     sttime = time.clock()
     image_files = os.listdir(input_images)
     i = 0
-    for filename in image_files:
+    for filename in image_files :
         image_path = input_images + os.path.sep + filename
         print(image_path)
         color_img = cv2.imread(image_path)
-		# converting color image to grayscale image
-        # print(color_img)
+        print(color_img)
+        # if(color_img is None) :
+        #     continue
+        # converting color image to grayscale image
         gray_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
 		# find the bounding boxes around detected faces in images
         bBoxes = frontal_face.detectMultiScale(gray_img, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
 		# print(bBoxes)
-        for box in bBoxes:
+        for box in bBoxes :
 			# print(box)
 			# crop and save the image at specified location
 			# cropImage(color_img, box)
@@ -97,7 +103,7 @@ def process_cropped_images(output_faces, subject_directory) :
         os.system("mv " + image_path + " " + new_path)
     print("Successfully completed the task in %.2f Secs." % (time.clock()- sttime))
 
-def get_images(path, size):
+def get_images(path, size) :
     '''
     path: path to a folder which contains subfolders of for each subject/person
         which in turn cotains pictures of subjects/persons.
@@ -108,8 +114,8 @@ def get_images(path, size):
     sub = 0
     images, labels = [], []
     people = []
-    for subdir in os.listdir(path):
-        for image in os.listdir(path + os.path.sep + subdir):
+    for subdir in os.listdir(path) :
+        for image in os.listdir(path + os.path.sep + subdir) :
             img= cv2.imread(path + os.path.sep + subdir + os.path.sep + image, cv2.IMREAD_GRAYSCALE)
             img= cv2.resize(img, size)
             images.append(numpy.asarray(img, dtype = numpy.uint8))
@@ -118,7 +124,7 @@ def get_images(path, size):
         sub += 1
     return [images, labels, people]
 
-def train_model(path):
+def train_model(path) :
     '''
     Takes path to images and train a face recognition model
     Returns trained model and people
@@ -133,7 +139,7 @@ def train_model(path):
     print("Successfully completed training in "+ str(time.clock()- sttime)+ " seconds!")
     return [eigen_model, people]
 
-def detect_faces(frontal_face, image):
+def detect_faces(frontal_face, image) :
     '''
     Takes an image as input and returns an array of bounding box(es).
     '''
@@ -146,54 +152,59 @@ def the_real_test(subject_directory, frontal_face, entity_name) :
              the Eigen Face Detection dataset
       OUTPUT: name of the person
     '''
-    eigen_model, people = train_model(subject_directory)
-    # print(people)
-    # starts recording video from camera and detects & predict subjects
-    sttime = time.clock()
-    cap = cv2.VideoCapture(0)
-    counter = 0
-    last_20 = [0 for i in range(20)]
-    final_5 = []
-    box_text = "Subject: "
-    while(True):
-        ret, frame = cap.read()
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray_frame = cv2.equalizeHist(gray_frame)
-        bBoxes = detect_faces(frontal_face, gray_frame)
-        for bBox in bBoxes:
-            (p,q,r,s) = bBox
-            cv2.rectangle(frame, (p, q), (p + r, q + s), (225, 0, 25), 2)
-            crop_gray_frame = gray_frame[q:q+s, p:p+r]
-            crop_gray_frame = cv2.resize(crop_gray_frame, (256, 256))
-            [predicted_label, predicted_conf] = eigen_model.predict(numpy.asarray(crop_gray_frame))
-            last_20.append(predicted_label)
-            last_20 = last_20[1:] # queue
-            cv2.putText(frame, box_text, (p-20, q-5), cv2.FONT_HERSHEY_PLAIN, 1.3, (25,0,225), 2)
-            '''
-            counter modulo x: changes value of final label for every x frames
-            Use max_label or predicted_label as you wish to see in the output video.
-            '''
-            if (counter % 10) == 0 :
-                max_label = majority(last_20)
-                box_text= format("Subject: " + people[max_label])
-                # timeout after a particular interval
-                if counter > 20 :
-                    final_5.append(max_label)
-                    # it always takes max_label into consideration
-                    print("[" + str(len(final_5)) + "] Detected face is: " + people[max_label])
-                    if len(final_5) == 5 :
-                        print("connection timed out...")
-                        if people[max_label] == entity_name :
-                            return True
-                        else :
-                            return False
-        cv2.imshow("Video Window", frame)
-        counter += 1
-        if (cv2.waitKey(5) & 0xFF == 27):
-            break
-    endtime = (time.clock()- sttime)
-    cap.release()
-    cv2.destroyAllWindows()
+    try :
+        eigen_model, people = train_model(subject_directory)
+        # print(people)
+        # starts recording video from camera and detects & predict subjects
+        sttime = time.clock()
+        cap = cv2.VideoCapture(0)
+        counter = 0
+        last_20 = [0 for i in range(20)]
+        final_5 = []
+        box_text = "Subject: "
+        while(True) :
+            ret, frame = cap.read()
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray_frame = cv2.equalizeHist(gray_frame)
+            bBoxes = detect_faces(frontal_face, gray_frame)
+            for bBox in bBoxes :
+                (p,q,r,s) = bBox
+                cv2.rectangle(frame, (p, q), (p + r, q + s), (225, 0, 25), 2)
+                crop_gray_frame = gray_frame[q:q+s, p:p+r]
+                crop_gray_frame = cv2.resize(crop_gray_frame, (256, 256))
+                [predicted_label, predicted_conf] = eigen_model.predict(numpy.asarray(crop_gray_frame))
+                last_20.append(predicted_label)
+                last_20 = last_20[1:] # queue
+                cv2.putText(frame, box_text, (p-20, q-5), cv2.FONT_HERSHEY_PLAIN, 1.3, (25,0,225), 2)
+                '''
+                counter modulo x: changes value of final label for every x frames
+                Use max_label or predicted_label as you wish to see in the output video.
+                '''
+                if (counter % 10) == 0 :
+                    max_label = majority(last_20)
+                    box_text= format("Subject: " + people[max_label])
+                    # timeout after a particular interval
+                    if counter > 20 :
+                        final_5.append(max_label)
+                        # it always takes max_label into consideration
+                        print("[" + str(len(final_5)) + "] Detected face is: " + people[max_label])
+                        if len(final_5) == 5 :
+                            print("connection timed out...")
+                            if people[max_label] == entity_name :
+                                return True
+                                raise GetOutOfLoop
+                            else :
+                                return False
+                                raise GetOutOfLoop
+            cv2.imshow("Video Window", frame)
+            counter += 1
+            if (cv2.waitKey(5) & 0xFF == 27):
+                break
+        endtime = (time.clock()- sttime)
+        cap.release()
+        cv2.destroyAllWindows()
+    except GetOutOfLoop :
+        pass
 
 def create_reqd_dirs(input_images, output_faces, subject_directory) :
     value = os.path.isdir(input_images)
